87b54840cbc6563a6517d628772741415e3d7ba3
yoon-wish <sowon5685@gmail.com>
2024-05-14 11:47:06 +0900

    Modify App Component using Theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />      
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
